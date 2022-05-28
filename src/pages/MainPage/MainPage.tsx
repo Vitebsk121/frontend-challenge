9f8f15d90e616a6cc30bb9f50c7c2879d9acecf7
Alexandr Demchenko <76862030+Vitebsk121@users.noreply.github.com>
2022-05-28 16:41:51 +0300

    feat: добавлена бесконечная прокрутка

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,43 +1,58 @@
-import "./MainPage.scss";
+import './MainPage.scss';
 
-import React, {useEffect, useState} from "react";
-import CardsList from "../../components/CardsList/CardsList";
-import axios from "axios";
+import React, { useEffect, useRef, useState } from 'react';
+import CardsList from '../../components/CardsList/CardsList';
+import axios from 'axios';
 
 type MainPageProps = {};
 
 type TApiData = {
   data: {
-    id: string
-    url: string
-  }[]
-}
+    id: string;
+    url: string;
+  }[];
+};
 
 const MainPage: React.FC<MainPageProps> = (props: MainPageProps) => {
-  const [cardData, setCardData] = useState<{url:string, id: string}[]>([]);
-  const [limit, setLimit] = useState(20);
+  const [cardData, setCardData] = useState<{ url: string; id: string }[]>([]);
+  const limit = 20;
   const [page, setPage] = useState(1);
+  const infinityEl = useRef<HTMLDivElement>(null);
+  const observer = useRef<IntersectionObserver>();
 
   const fetchData = async (limit: number, page: number) => {
     try {
-      axios.defaults.headers.common['x-api-key'] = "a8b46641-6368-48b9-a8cf-7687061155e9";
-      let response: TApiData = await axios.get('https://api.thecatapi.com/v1/images/search', { params: { limit:limit, page:page, size:"full" } } );
-      setCardData(prev => [...prev, ...response.data]);
+      axios.defaults.headers.common['x-api-key'] = 'a8b46641-6368-48b9-a8cf-7687061155e9';
+      let response: TApiData = await axios.get('https://api.thecatapi.com/v1/images/search', {
+        params: { limit: limit, page: page, size: 'full' },
+      });
+      setCardData((prev) => [...prev, ...response.data]);
     } catch (e) {
-      alert(e)
+      alert(e);
     }
-  }
+  };
+
+  useEffect(() => {
+    const callback = function (entries: { isIntersecting: any }[]) {
+      if (entries[0].isIntersecting) {
+        setPage((prev) => prev + 1);
+      }
+    };
+    if (infinityEl.current === null) return;
+    observer.current = new IntersectionObserver(callback);
+    observer.current.observe(infinityEl.current);
+  }, []);
 
   useEffect(() => {
     fetchData(limit, page);
-  }, [page])
+  }, [page]);
   return (
     <div className="content">
       <div className="wrapper">
-        {cardData.length === 0
-          ? (<h3 className='loader'>... загружаем еще котиков ...</h3>)
-          : (<CardsList cardData={cardData}/>)
-        }
+        <CardsList cardData={cardData} />
+        <div ref={infinityEl} className="infinity-scroll__el">
+          <h3 className="loader">... загружаем еще котиков ...</h3>
+        </div>
       </div>
     </div>
   );
