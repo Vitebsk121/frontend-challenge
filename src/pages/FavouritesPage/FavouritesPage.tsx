81f422706ecfcb36e463b992eb9ca6c1cedf2302
Alexandr Demchenko <76862030+Vitebsk121@users.noreply.github.com>
2022-05-28 16:04:04 +0300

    feat: добавлена страница с избранными котиками

---
+++ src/pages/FavouritesPage/FavouritesPage.tsx
@@ -0,0 +1,37 @@
+import "./FavouritesPage.scss";
+
+import React, {useEffect, useState} from "react";
+import CardsList from "../../components/CardsList/CardsList";
+
+type FavouritesProps = {};
+
+const FavouritesPage: React.FC<FavouritesProps> = (props: FavouritesProps) => {
+  const [cardData, setCardData] = useState<{url:string, id: string}[]>([]);
+
+  useEffect(() => {
+    const arrOfCards = [];
+    for(let i = 0; i < localStorage.length; i++) {
+      let key = localStorage.key(i);
+      if (!key) return;
+      let card = {
+        id: key,
+        url: localStorage.getItem(key) || '',
+      };
+      arrOfCards.push(card);
+    }
+    setCardData(arrOfCards);
+  }, [])
+
+  return (
+    <div className="content">
+      <div className="wrapper">
+        {cardData.length === 0
+          ? (<h3 className='loader'>Добавьте котиков в избранное</h3>)
+          : (<CardsList cardData={cardData}/>)
+        }
+      </div>
+    </div>
+  );
+};
+
+export default FavouritesPage;
